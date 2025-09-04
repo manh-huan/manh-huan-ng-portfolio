854795dbfc68b36f0b694ec5ef5c4da56422fda2
Mạnh Huân Nguyễn <manhhuannguyen@MBP-de-Manh.lan>
2025-09-05 01:20:12 +0200

    try to fix photo missing

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -24,7 +24,7 @@ export class ProjectsComponent {
     {
       title: 'Submeowrine',
       href: 'https://github.com/yujisatojr/submeowrine',
-      imgSrc: 'assets/images/mock01.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock01.png',
       alt: 'thumbnail',
       description:
         'Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.'
@@ -32,7 +32,7 @@ export class ProjectsComponent {
     {
       title: 'WeManage: Real Estate Asset Management',
       href: 'http://www.wemanage.jp/',
-      imgSrc: 'assets/images/mock06.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock06.png',
       alt: 'thumbnail',
       nowrapTitle: true,
       description:
@@ -41,7 +41,7 @@ export class ProjectsComponent {
     {
       title: 'COVID-19 Case Management',
       href: 'https://www.byuh.edu/covid-19-case-management',
-      imgSrc: 'assets/images/mock05.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock05.png',
       alt: 'thumbnail',
       description:
         'Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university\'s leadership in their decision-making processes.'
@@ -49,7 +49,7 @@ export class ProjectsComponent {
     {
       title: 'Multiple Regression Property Analysis',
       href: 'https://github.com/yujisatojr/multi-reg-analysis',
-      imgSrc: 'assets/images/mock04.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock04.png',
       alt: 'thumbnail',
       description:
         'Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.'
@@ -57,7 +57,7 @@ export class ProjectsComponent {
     {
       title: 'Programs of Study',
       href: 'https://holokai.byuh.edu/programs-of-study',
-      imgSrc: 'assets/images/mock03.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock03.png',
       alt: 'thumbnail',
       description:
         'Designed and developed a custom component for a CMS-based platform (e.g., \'Brightspot\') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.'
@@ -65,7 +65,7 @@ export class ProjectsComponent {
     {
       title: 'Transfer Evaluation Matrix',
       href: 'https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix',
-      imgSrc: 'assets/images/mock02.png',
+      imgSrc: '/manh-huan-ng-portfolio-/assets/images/mock02.png',
       alt: 'thumbnail',
       description:
         'Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.'
