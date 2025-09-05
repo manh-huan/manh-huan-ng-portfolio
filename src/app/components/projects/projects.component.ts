0b8c4c178ebf93fc7b6fa3d6694a0f1fa8c06d0f
Mạnh Huân Nguyễn <manhhuannguyen@MBP-de-Manh.lan>
2025-09-06 00:40:21 +0200

    change project section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -20,55 +20,20 @@ interface ProjectItem {
 })
 export class ProjectsComponent {
   projects: ProjectItem[] = [
-
     {
-      title: 'Submeowrine',
-      href: 'https://github.com/yujisatojr/submeowrine',
-      imgSrc: '/manh-huan-ng-portfolio/assets/images/mock01.png',
-      alt: 'thumbnail',
-      description:
-        'Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.'
-    },
-    {
-      title: 'WeManage: Real Estate Asset Management',
-      href: 'http://www.wemanage.jp/',
-      imgSrc: '/manh-huan-ng-portfolio/assets/images/mock06.png',
-      alt: 'thumbnail',
-      nowrapTitle: true,
-      description:
-        'This mobile application allows realtors in Japan to securely manage their property information and view future income predictions. This app is built with Ruby on Rails and JavaScript.'
-    },
-    {
-      title: 'COVID-19 Case Management',
-      href: 'https://www.byuh.edu/covid-19-case-management',
-      imgSrc: '/manh-huan-ng-portfolio/assets/images/mock05.png',
-      alt: 'thumbnail',
-      description:
-        'Built official charts for COVID/vaccination tracking for an educational institution using JavaScript and the Google Sheets API v4. The dashboard served the university\'s leadership in their decision-making processes.'
-    },
-    {
-      title: 'Multiple Regression Property Analysis',
-      href: 'https://github.com/yujisatojr/multi-reg-analysis',
-      imgSrc: '/manh-huan-ng-portfolio/assets/images/mock04.png',
-      alt: 'thumbnail',
-      description:
-        'Analyzed the real estate market in Japan and predicted property prices by implementing statistical methods such as OLS and multi-regression analysis. This project leveraged Python and various libraries such as Pandas, NumPy, Matplotlib, and Scikit-Learn.'
-    },
-    {
-      title: 'Programs of Study',
-      href: 'https://holokai.byuh.edu/programs-of-study',
+      title: 'CompDoc - Academic Report Analyzer',
+      href: 'https://github.com/rchacons/academic-report-analyzer',
       imgSrc: '/manh-huan-ng-portfolio/assets/images/mock03.png',
       alt: 'thumbnail',
       description:
-        'Designed and developed a custom component for a CMS-based platform (e.g., \'Brightspot\') using Java, Handlebars, and LESS. University students can find their majors of interest through this module.'
+        'A platform with FastAPI Backend and React Frontend that analyzes, compare and extract academic report.'
     },
     {
-      title: 'Transfer Evaluation Matrix',
-      href: 'https://hookele.byuh.edu/transfer-evaluation-guidelines-and-matrix',
+      title: 'Java Banking App',
+      href: 'https://github.com/manh-huan/mahu-java-banking-app',
       imgSrc: '/manh-huan-ng-portfolio/assets/images/mock02.png',
       alt: 'thumbnail',
-      description:
-        'Created an interactive CSV table generator with Java, Handlebars, and LESS. This project helps transfer students to quickly identify eligible credits.'
-    },
+      description: 'A simple banking application built with Java FX which provides a user-friendly interface for managing accounts, transactions, and dashboards.'
+      },
   ];
 }
